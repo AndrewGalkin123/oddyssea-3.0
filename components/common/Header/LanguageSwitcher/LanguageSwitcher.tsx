0a377c37dc8e2091c547f338d92ab22a35cf6dc2
Andrew <127788127788a@gmail.com>
2024-01-24 21:32:38 +0200

    creating LanguageSwitcher

---
+++ components/common/Header/LanguageSwitcher/LanguageSwitcher.tsx
@@ -0,0 +1,14 @@
+"use client"
+import Link from "next/link";
+
+const LanguageSwitcher: React.FC = () => {
+
+
+  return (
+    <div>
+       <a href="/">En</a>
+    </div>
+  );
+};
+
+export default LanguageSwitcher;
