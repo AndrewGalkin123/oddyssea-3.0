6870b4d7c961737a5784e0c1bc6313c85bc84269
Andrew <127788127788a@gmail.com>
2024-01-27 22:59:50 +0200

    trying to fix bug

--- components/common/Header/LanguageSwitcher/LanguageSwitcher.tsx
+++ components/common/Header/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,40 +1,40 @@
 "use client";
-
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import Link from 'next/link';
 
 const LanguageSwitcher: React.FC = () => {
-  const [currentLanguage, setCurrentLanguage] = useState<string>("ru");
-  
+  const languages = ["uk", "en", "ru"];
+  const [currentLanguageIndex, setCurrentLanguageIndex] = useState<number>(0);
+
+  const getNextLanguageIndex = () => {
+    let newIndex = currentLanguageIndex + 1;
+
+    // Handle the case where newIndex goes beyond the length of languages array
+    if (newIndex >= languages.length) {
+      newIndex = 0;
+    }
+
+    return newIndex;
+  };
 
   const handleTranslate = () => {
-    switch (currentLanguage) {
-      case "ru":
-        window.location.href = "/uk";
-        setCurrentLanguage("uk");
-        break;
-      case "uk":
-        window.location.href = "/en";
-        setCurrentLanguage("en");
-        break;
-      case "en":
-        window.location.href = "/ru";
-        setCurrentLanguage("ru");
-        break;
-      default:
-        setCurrentLanguage("ru");
-        break;
-    }
+    const newIndex = getNextLanguageIndex();
+    setCurrentLanguageIndex(newIndex);
   };
 
   return (
     <div>
-       <button className="translator-button" onClick={handleTranslate}>{currentLanguage === "ru"
-        ? "UK"
-        : currentLanguage === "uk"
-          ? "EN"
-          : "RU"}</button>
+      <button className="translator-button" onClick={handleTranslate}>
+        <Link href={`/${languages[currentLanguageIndex]}`}>
+          {languages[currentLanguageIndex].toUpperCase()}
+        </Link>
+      </button>
     </div>
   );
 };
 
 export default LanguageSwitcher;
+
+
+
+
