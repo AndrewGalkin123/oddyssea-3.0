4b532a9ef855ea4c7150e4b7f3f4a299e5e6b591
Andrew <127788127788a@gmail.com>
2024-02-16 22:09:48 +0200

    # logic of changing content

--- src/app/[locale]/culture/page.tsx
+++ src/app/[locale]/culture/page.tsx
@@ -1,17 +1,30 @@
+"use client";
 import { Row, Col } from "antd";
 import { ContentMenu } from "../../../../components/CulturePageComponents/ContentMenu/ContentMenu";
 import { Content } from "../../../../components/CulturePageComponents/Content/Content";
-
+import { useTranslations } from "next-intl";
 
 export default function Culture() {
+  const handleMenuItemClick = (key: string) => {
+    return key;
+  };
+  const t = useTranslations(handleMenuItemClick);
   return (
     <main style={{ padding: "100px 0px" }}>
       <Row>
         <Col md={3}>
-          <ContentMenu></ContentMenu>
+          <ContentMenu onItemClick={handleMenuItemClick}></ContentMenu>
         </Col>
         <Col md={21}>
-          <Content title="Опера" firstParagraph="Одесская Опера" secondParagraph="123123" thirdParagraph="213123" src="/assets/historyComponent/opera.jpg"/>
+          <Content
+            title="Опера"
+            firstParagraph="Одесская Опера"
+            secondParagraph="123123"
+            thirdParagraph="213123"
+            fourthParagraph="1212"
+            fifthParagraph="d1"
+            src="/assets/historyComponent/opera.jpg"
+          />
         </Col>
       </Row>
     </main>
