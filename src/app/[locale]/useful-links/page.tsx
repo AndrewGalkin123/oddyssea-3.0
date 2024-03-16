e50f9ce94426122ee9577ffcae1c0b27d583ea7d
Andrew <127788127788a@gmail.com>
2024-03-16 21:19:30 +0200

    useful links page + added translations for odessa map

---
+++ src/app/[locale]/useful-links/page.tsx
@@ -0,0 +1,10 @@
+import UsefulLinkContent from "../../../../components/UsefulLinksPageComponents/UsefulLinkContent/UsefulLinkContent";
+
+
+export default function UsefulLinks(){
+    return(
+        <main style={{ padding: "100px" }}>
+           <UsefulLinkContent/>
+        </main>
+    )
+}
