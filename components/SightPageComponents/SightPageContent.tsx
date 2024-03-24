6f767822aeb1594ea8ea4f3c5a8a535d2b26024f
Andrew <127788127788a@gmail.com>
2024-03-24 12:14:55 +0200

    finished 360 frames for each history sight

--- components/SightPageComponents/SightPageContent.tsx
+++ components/SightPageComponents/SightPageContent.tsx
@@ -2,6 +2,7 @@
 import { MutableRefObject, useRef } from "react";
 import { SightHelpButtons } from "./SightHelpButtons";
 import { useTranslations } from "next-intl";
+import historyContent from "../../public/content/historyContent.json"
 
 
 interface SightPageContentProps {
@@ -22,7 +23,7 @@ const SightPageContent: React.FC<SightPageContentProps> = ({ sight }) => {
     <div>
       <iframe
         className="location"
-        src="https://www.google.com/maps/embed?pb=!4v1690799078512!6m8!1m7!1sCAoSLEFGMVFpcE1oUmJ6WHRHdUJUSVVMZU5rbklZZUh5YzVkMDNCZUVyT29hSk9u!2m2!1d46.485071!2d30.7439437!3f4.180024503728842!4f2.3371492776826983!5f0.7820865974627469"
+        src={`https://www.google.com/maps/embed?pb=${historyContent[sight].frame}`}
         loading="lazy"
       ></iframe>
       <SightHelpButtons onClick={handleScrollButtonClick} />
