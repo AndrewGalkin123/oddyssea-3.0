778ef4d0825c89b2a389e18c64e7496570eb24d2
Andrew <127788127788a@gmail.com>
2024-04-05 22:19:24 +0300

    added floatbutton

--- components/PhotoGalleryPageComponents/PhotoGalleryContent/PhotoGalleryContent.tsx
+++ components/PhotoGalleryPageComponents/PhotoGalleryContent/PhotoGalleryContent.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { PhotoGalleryFilter } from "../Filter/PhotoGalleryFilter";
-import { Flex, Image } from "antd";
+
+import { Flex, Image, FloatButton } from "antd";
 
 interface PhotoGalleryContentProps { 
     arr: Array<string>;
@@ -16,7 +16,7 @@ const PhotoGalleryContent:React.FC<PhotoGalleryContentProps> = ({arr}) => {
                         arr.map((item => <Image style={{objectFit: "cover"}} key={item} width="250px" height="250px" src={item}/>))
                     }
                 </Flex>
-            
+                <FloatButton.BackTop/>
         </div>
     )
 }
